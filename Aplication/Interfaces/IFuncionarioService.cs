db8d8ecd6850e72dc9de4d36e57dba22c135b892
dieisson <dieisson.qfloriano@gmail.com>
2026-08-19 23:01:31 -0300

    adicionado codigo classe funcionario

--- Aplication/Interfaces/IFuncionarioService.cs
+++ Aplication/Interfaces/IFuncionarioService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Application.Interfaces
 {
-    internal class FuncionarioService
+    internal class IFuncionarioService
     {
     }
 }
--- Domain/Entities/Funcionario.cs
+++ Domain/Entities/Funcionario.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    internal class Funcionario
+    public class Funcionario
     {
+        
+        
+            public int Id { get; set; }
+
+            [Required]
+            public string Nome { get; set; } = string.Empty;
+
+            [Required]
+            public string Cargo { get; set; } = string.Empty;
+
+            [Range(0.01, double.MaxValue)]
+            public decimal Salario { get; set; }
+
+            [Required]
+            public string Departamento { get; set; } = string.Empty;
+
+            public bool Ativo { get; set; }
+        
     }
 }
