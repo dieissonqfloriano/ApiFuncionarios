80a58c5177198c942a758f25ff36c7b271729e7b
dieisson <dieisson.qfloriano@gmail.com>
2026-08-20 09:17:28 -0300

    adicionado configuracao de coneccao com a banco

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,15 +1,26 @@
+using Application.Interfaces;
+using Application.Services;
+using Infrastruture.Data;
+using Infrastruture.Repositories;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IFuncionarioRepository, FuncionarioRepository>();
+builder.Services.AddScoped<IFuncionarioService, FuncionarioService>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
