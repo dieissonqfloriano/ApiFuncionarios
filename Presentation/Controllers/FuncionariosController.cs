995069faccd97bf09ca02806784b90ad4982cbcb
dieisson <dieisson.qfloriano@gmail.com>
2026-08-20 08:38:47 -0300

    adicionado FuncionariosController

--- Presentation/Controllers/FuncionariosController.cs
+++ Presentation/Controllers/FuncionariosController.cs
@@ -1,6 +1,26 @@
-﻿namespace Presentation.Controllers
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
 {
-    public class FuncionariosController
+    [ApiController]
+    [Route("api/funcionarios")]
+    public class FuncionariosController : ControllerBase
     {
+        private readonly IFuncionarioService _service;
+
+        public FuncionariosController(IFuncionarioService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Criar(FuncionarioInputDto dto)
+        {
+            var funcionario = await _service.CreateAsync(dto);
+
+            return StatusCode(201, funcionario);
+        }
     }
 }
