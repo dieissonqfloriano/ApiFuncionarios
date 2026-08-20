e24259985f8480ec6c2218223ce01bb923171dad
dieisson <dieisson.qfloriano@gmail.com>
2026-08-20 19:45:19 -0300

    adicionado os end points no controller

--- Presentation/Controllers/FuncionariosController.cs
+++ Presentation/Controllers/FuncionariosController.cs
@@ -15,12 +15,55 @@ namespace Presentation.Controllers
             _service = service;
         }
 
+        
+        [HttpGet]
+        public async Task<IActionResult> Listar()
+        {
+            var funcionarios = await _service.GetAllAsync();
+
+            return Ok(funcionarios);
+        }
+
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> BuscarPorId(int id)
+        {
+            var funcionario = await _service.GetByIdAsync(id);
+
+            if (funcionario == null)
+                return NotFound();
+
+            return Ok(funcionario);
+        }
+
+        
         [HttpPost]
         public async Task<IActionResult> Criar(FuncionarioInputDto dto)
         {
             var funcionario = await _service.CreateAsync(dto);
 
             return StatusCode(201, funcionario);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Atualizar(
+            int id,
+            FuncionarioInputDto dto)
+        {
+            var funcionario = await _service.UpdateAsync(id, dto);
+
+            if (funcionario == null)
+                return NotFound();
+
+            return Ok(funcionario);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Excluir(int id)
+        {
+            await _service.DeleteAsync(id);
+
+            return NoContent();
         }
     }
 }
